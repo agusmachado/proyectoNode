d10395f4cd1219bfcaad6793791310a7bd089a81
agusmachado <agusmachado@gmail.com>
2023-08-08 10:43:22 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const evenOrOdd = require('num-is-even-or-odd');
+
+console.log(evenOrOdd(25))
+console.log('Este número es del tipo', evenOrOdd(87))
+console.log('buenas noches')
+/* console.log('Hola mundo')
+
+function saludo(nombre) {
+    console.log(`hola buenos días ${nombre} `)
+}
+saludo('manuel') */
